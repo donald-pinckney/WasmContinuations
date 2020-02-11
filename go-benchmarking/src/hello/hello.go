f6a3328744a425e93953c52d04176acd45764596
Donald Pinckney <donald_pinckney@icloud.com>
2020-02-11 14:41:39 -0500

    x

--- go-benchmarking/src/hello/hello.go
+++ go-benchmarking/src/hello/hello.go
@@ -3,26 +3,34 @@ package main
 import "fmt"
 import "strconv"
 import "os"
-import "runtime"
+// import "runtime"
 
 func main() {
 	a, _ := strconv.Atoi(os.Args[1])
 	b, _ := strconv.Atoi(os.Args[2])
 
-	ch := make(chan float64)
+	// ch := make(chan float64)
+	// stuff(ch, a, b)
+	// go stuff(ch, a+7, b+987234)
 
-	fmt.Println(stuff(ch, a, b));
+	fmt.Println(stuff(a, b));
 }
 
 //go:noinline
-func stuff(ch chan float64, x int, y int) float64 {
+func other_thing() {
+	// runtime.Gosched()
+}
+
+//go:noinline
+func stuff(x int, y int) float64 {
 	z := x + y
 	z = z + 6
 	s := 0.0
 	for i := 0; i < z; i++ {
 		f := float64(i + i*i);
-		runtime.Gosched()
+		other_thing()
 		s += f;
 	}
+	
 	return s
 }
