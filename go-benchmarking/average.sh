b54f8992c3f87e6df2a6812afd6e2d6261cf3668
Donald Pinckney <donald_pinckney@icloud.com>
2020-02-14 15:44:05 -0500

    Split go-benchmarking into submodule

--- go-benchmarking/average.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-n=$1
-shift
-t=$({ time -p for i in `seq $n`; do $@; done 2>/dev/null 1>/dev/null ; } 2>&1 | head -1 | cut -d' ' -f2)
-echo "scale=4; $t/$n" | bc
-# echo $t
--- go-benchmarking/pi_wasm_wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$(go env GOROOT)/misc/wasm/go_js_wasm_exec bin/js_wasm/pi $1 $2 $3
