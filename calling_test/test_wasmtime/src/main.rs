bf6babfebc0af733618a89b72cb3f091b22da52f
Donald Pinckney <donald_pinckney@icloud.com>
2020-04-21 18:50:25 -0400

    Test interleaving of Rust or JS and Wasm

---
+++ calling_test/test_wasmtime/src/main.rs
@@ -0,0 +1,126 @@
+//! Translation of hello example
+
+extern crate alloc;
+
+// use alloc::rc::Rc;
+use core::cell::Ref;
+use failure::{bail, format_err, Error};
+use std::fs::read;
+use wasmtime_api::*;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+struct PrintCallback(i32);
+
+
+static mut __THE_INST: Option<HostRef<Instance>> = None;
+// static mut THE_INST: Option<&'static mut HostRef<Instance>> = None;
+static mut STUFF: Option<&HostRef<Func>> = None;
+
+fn get_inst() -> &'static mut HostRef<Instance> {
+    unsafe { 
+        match &mut __THE_INST {
+            Some(x) => x,
+            None => panic!(),
+        } 
+    }
+}
+
+fn set_inst(i: HostRef<Instance>) {
+    unsafe { __THE_INST = Some(i); }
+}
+
+impl Callable for PrintCallback {
+    fn call(&self, params: &[Val], _results: &mut [Val]) -> Result<(), HostRef<Trap>> {
+        // let STUFF_val = unsafe { STUFF };
+        let exports = Ref::map(get_inst().borrow(), |instance| instance.exports());
+        let inc_func = exports[1].func().unwrap();
+
+        match params[0] {
+            Val::I32(x) => {
+                match inc_func.borrow().call(&[Val::I32(x)]).expect("bad")[0] {
+                    Val::I32(inc_res) => {
+                        println!("> {}", inc_res)
+                    },
+                    _ => panic!()
+                }
+            },
+            _ => panic!("Wrong argument type")
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Error> {
+    // Initialize.
+    println!("Initializing...");
+    let engine = HostRef::new(Engine::new(Config::default()));
+    let store = HostRef::new(Store::new(engine));
+
+    // Load binary.
+    println!("Loading binary...");
+    let binary = read("hello.wasm")?;
+
+    // Compile.
+    println!("Compiling module...");
+    let module = HostRef::new(
+        Module::new(store.clone(), &binary)
+            .map_err(|_| format_err!("> Error compiling module!"))?,
+    );
+
+    // Create external print functions.
+    println!("Creating callback...");
+    let print_type = FuncType::new(Box::new([ValType::I32]), Box::new([]));
+
+    let callback = PrintCallback(5);
+    let callback_rc = Rc::new(callback);
+    // callback_rc.modify();
+    // {
+    //     // let mut d = callback_rc.borrow_mut();
+    // }
+    let print_func = HostRef::new(Func::new(store.clone(), print_type, callback_rc));
+    // {
+    //     let mut d = print_func.borrow_mut();
+
+    // }
+
+    // Instantiate.
+    println!("Instantiating module...");
+    let imports = vec![print_func.into()];
+    set_inst(HostRef::new(
+        Instance::new(store.clone(), module, imports.as_slice()).map_err(|_| format_err!("> Error instantiating module!"))?
+    ));
+
+    // Extract export.
+    println!("Extracting export...");
+    // let test = unsafe { THE_INST.unwrap().borrow() };
+    let exports = Ref::map(get_inst().borrow(), |instance| instance.exports());
+    if exports.len() == 0 {
+        bail!("> Error accessing exports!");
+    }
+    let run_func = exports[0]
+        .func()
+        .ok_or_else(|| format_err!("> Error accessing exports!"))?;
+
+    // let inc_func = exports[1]
+    //     .func()
+    //     .ok_or_else(|| format_err!("> Error accessing exports!"))?;
+
+    // let inc_f = run_func.borrow();
+
+    // unsafe { STUFF = Some(inc_func); }
+
+    // Call.
+    println!("Calling export...");
+    if let Err(_) = run_func.borrow().call(&[]) {
+        bail!("> Error calling function!");
+    }
+
+    // Shut down.
+    println!("Shutting down...");
+    drop(store);
+
+    // All done.
+    println!("Done.");
+    Ok(())
+}
---
+++ calling_test/web/test.wat
@@ -0,0 +1,14 @@
+;; simple.wasm
+(module
+    (func $print_i32 (import "imports" "print_i32") (param i32))
+    (func (export "main")
+        i32.const 42
+        call $print_i32
+    )
+
+    (func (export "increment") (param i32) (result i32)
+        i32.const 1
+        local.get 0
+        i32.add
+    )
+)
